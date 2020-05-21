6803249538317de10aa82ca8548a2352529620a9
CO18344 <satviksingh28@gmail.com>
2020-05-21 13:38:04 +0530

    Dictionary methods

---
+++ dic.py
@@ -0,0 +1,12 @@
+def getKeysValues(dicObj):
+	return list(dicObj.keys()),list(dicObj.values())
+def getItemsFromDic(dicObj):
+	return list(dicObj.items)
+
+if __name__ == '__main__':
+	x ={'name':'satvik',
+	'gender':'male'}
+	print(x)
+	keys,values = getKeysValues(x)
+	print(keys)
+	print(values)
